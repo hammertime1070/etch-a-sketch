d3672dd95acdd6b16e4a04defe2b9a2f62c23ba1
hammertime1070 <mbkeith614@gmail.com>
2023-08-25 10:06:15 -0400

    Implement 16x16 grid using displayGame and displayColumn functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function displayGame() {
+    let i = 0;
+    for (let i = 0; i < 16; i++){
+        const game = document.querySelector('#game')
+        const column = document.createElement('div')
+        column.classList.add('column')
+        column.style.display = 'flex'
+        column.style.flexDirection = 'column'
+        displayColumn(column)
+        game.appendChild(column)
+    }
+}
+
+function displayColumn(column) {
+    for (let i = 0; i < 16; i++) {
+        const tile = document.createElement('div')
+        tile.classList.add('tile')
+        tile.textContent = 'Hello World'
+        column.appendChild(tile)
+    }
+}
+
+
+displayGame()
